6490f672f7843a82af20f34631bd403bee919c1e
reng <reng412@gmail.com>
2020-11-16 20:59:13 -0800

    Add wrapper component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Projects from "./projects.json";
 import ProjectContext from "./utils/projectContext";
 import Project from "../src/components/Project";
+import Wrapper from "../src/components/Wrapper";
 import Header from "../src/components/Header";
 import Jumbotron from "../src/components/Jumbotron";
 import Footer from "../src/components/Footer";
@@ -17,14 +19,18 @@ function App() {
     }, []);
 
     return (
+        // <Router>
         <div>
             <Header></Header>
             <Jumbotron></Jumbotron>
-            <ProjectContext.Provider value={projects}>
-                <Project></Project>
-            </ProjectContext.Provider>
+            <Wrapper>
+                <ProjectContext.Provider value={projects}>
+                    <Project></Project>
+                </ProjectContext.Provider>
+            </Wrapper>
             <Footer></Footer>
         </div >
+        // </Router>
     );
 }
 
