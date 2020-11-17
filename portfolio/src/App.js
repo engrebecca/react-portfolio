a8ac62faca4ecdc85120fd5a4b5aef5ab0f462c3
reng <reng412@gmail.com>
2020-11-16 19:26:07 -0800

    Add projects.jsjson file

---
+++ portfolio/src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+    // useState hook
+    // const [employees, setEmployees] = useState([]);
+
+    // useEffect hook that sets employees array to be the array from employees.json when page mounts
+    // useEffect(() => {
+    //     setEmployees(Employees)
+    // }, []);
+
+    return (
+        <div>
+            <p>Hello</p>
+        </div >
+    );
+}
+
+export default App;
