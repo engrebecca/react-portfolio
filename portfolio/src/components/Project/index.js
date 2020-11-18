fb905fa24570c792f8af98f011ba898926fac469
reng <reng412@gmail.com>
2020-11-17 18:11:59 -0800

    Update css styles on portfolio page for font and spacing

--- portfolio/src/components/Project/index.js
+++ portfolio/src/components/Project/index.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import ProjectContext from "../../utils/projectContext";
-import "./style.css";
 
 function Project() {
     const projects = useContext(ProjectContext);
@@ -14,13 +13,13 @@ function Project() {
                         <div className="card-group">
                             <div className="card m-3 border-0">
                                 <>{projects.map((project, i) => (
-                                    <div className="container text-center" key={i}>
+                                    <div className="container text-center project mb-5" key={i}>
                                         <a href={project.deployLink} target="_blank"><img className="card-img-top border border-dark" src={project.image} alt={project.title} data-aos="fade-up" data-aos-delay="50" /></a>
                                         <h4 className="text-center m-4 cormorant-font sub-text">{project.title}</h4>
                                         <p className="cormorant-font text-center px-5">{project.description}</p>
-                                        <span className="mb-5">
-                                            <a href={project.githubLink} className="text-secondary m-2 cormorant-font" target="_blank">GitHub</a>
-                                            <a href={project.deployLink} className="text-secondary m-2 cormorant-font" target="_blank">See It Online</a>
+                                        <span>
+                                            <a href={project.githubLink} className="text-secondary cormorant-font" target="_blank">GitHub</a>
+                                            <a href={project.deployLink} className="text-secondary cormorant-font" target="_blank">See It Online</a>
                                         </span>
                                     </div>
                                 ))
