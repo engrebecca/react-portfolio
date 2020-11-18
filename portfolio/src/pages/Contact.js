736a19ca2c6b026dba36af5f37c5203a14d36e7b
reng <reng412@gmail.com>
2020-11-17 19:21:57 -0800

    Update contact page w/ icons and links

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -3,30 +3,58 @@ import React from "react";
 function Contact() {
     return (
         <main className="flex-shrink-0 content">
-            <div className="container mt-5 mb-5">
-                <div className="row">
-                    <div className="col d-flex">
-                        <div className="card p-3" style={{ width: "40rem" }} data-aos="fade-down" data-aos-delay="50">
+            {/* Let's get in touch sub title */}
+            <div className="container">
+                <h1 className="border-bottom cormorant-font sub-text text-center" data-aos="fade-down"
+                    data-aos-delay="50">Let's get in touch!</h1>
+            </div>
+            <div className="container mt-5 mb-5 text-center">
+                <div class="row">
+                    <div class="col-sm" data-aos="fade-down" data-aos-delay="50">
+                        {/* Email */}
+                        <div className="card border-0" style={{ width: "18rem" }}>
                             <div className="card-body">
-                                <form>
-                                    <div className="form-group">
-                                        <label htmlFor="form-name" className="cormorant-font">Name</label>
-                                        <input type="text" className="form-control cormorant-font" id="form-name"
-                                            placeholder="Name" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="form-email" className="cormorant-font">Email address</label>
-                                        <input type="email" className="form-control cormorant-font" id="form-email"
-                                            placeholder="Email" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="form-message" className="cormorant-font">Message</label>
-                                        <textarea className="form-control cormorant-font" id="form-message" rows="3"
-                                            placeholder="Message"></textarea>
-                                    </div>
-                                    <button type="submit"
-                                        className="btn btn-primary btn-secondary cormorant-font">Submit</button>
-                                </form>
+                                <a href="mailto:reng412@gmail.com" className="text-body"><i className="far fa-envelope fa-5x"></i></a>
+                                <p className="card-text cormorant-font">reng412@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Phone */}
+                    <div class="col-sm" data-aos="fade-down" data-aos-delay="50">
+                        <div className="card border-0" style={{ width: "18rem" }}>
+                            <div className="card-body">
+                                <i className="fas fa-phone fa-5x"></i>
+                                <p className="card-text cormorant-font">(510) 473-2267</p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Resume */}
+                    <div class="col-sm" data-aos="fade-down" data-aos-delay="50">
+                        <div className="card border-0" style={{ width: "18rem" }}>
+                            <div className="card-body">
+                                <a href="Eng Rebecca Resume_11.7.20.pdf" target="_blank" rel="noreferrer" className="text-body">
+                                    <i className="far fa-file fa-5x"></i></a>
+                                <p className="card-text cormorant-font">Resume</p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* LinkedIn */}
+                    <div class="col-sm" data-aos="fade-down" data-aos-delay="50">
+                        <div className="card border-0" style={{ width: "18rem" }}>
+                            <div className="card-body">
+                                <a href="https://www.linkedin.com/in/engrebecca/" target="_blank" rel="noreferrer" className="text-body"><i
+                                    className="fab fa-linkedin fa-5x"></i></a>
+                                <p className="card-text cormorant-font">LinkedIn</p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* GitHub */}
+                    <div class="col-sm" data-aos="fade-down" data-aos-delay="50">
+                        <div className="card border-0" style={{ width: "18rem" }}>
+                            <div className="card-body">
+                                <a href="https://github.com/engrebecca" target="_blank" rel="noreferrer" className="text-body"><i
+                                    className="fab fa-github fa-5x"></i></a>
+                                <p className="card-text cormorant-font">GitHub</p>
                             </div>
                         </div>
                     </div>
