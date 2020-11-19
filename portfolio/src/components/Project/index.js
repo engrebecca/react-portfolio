abaa91ea1f3b30bc1993952961522d6730827b01
reng <reng412@gmail.com>
2020-11-18 18:08:39 -0800

    add noreferrer to target blanks on project component

--- portfolio/src/components/Project/index.js
+++ portfolio/src/components/Project/index.js
@@ -14,12 +14,12 @@ function Project() {
                             <div className="card m-3 border-0">
                                 <>{projects.map((project, i) => (
                                     <div className="container text-center project mb-5" key={i}>
-                                        <a href={project.deployLink} target="_blank"><img className="card-img-top border border-dark" src={project.image} alt={project.title} data-aos="fade-up" data-aos-delay="50" /></a>
+                                        <a href={project.deployLink} target="_blank" rel="noreferrer"><img className="card-img-top border border-dark" src={project.image} alt={project.title} data-aos="fade-up" data-aos-delay="50" /></a>
                                         <h4 className="text-center m-4 cormorant-font sub-text">{project.title}</h4>
                                         <p className="cormorant-font text-center px-5">{project.description}</p>
                                         <span>
-                                            <a href={project.githubLink} className="text-secondary cormorant-font" target="_blank">GitHub</a>
-                                            <a href={project.deployLink} className="text-secondary cormorant-font" target="_blank">See It Online</a>
+                                            <a href={project.githubLink} className="text-secondary cormorant-font" target="_blank" rel="noreferrer">GitHub</a>
+                                            <a href={project.deployLink} className="text-secondary cormorant-font" target="_blank" rel="noreferrer">See It Online</a>
                                         </span>
                                     </div>
                                 ))
