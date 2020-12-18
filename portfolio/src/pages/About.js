3c6fa1e1420c7a41a602086e0a6d26d1d86c137e
reng <reng412@gmail.com>
2020-12-18 15:49:08 -0800

    Update about component to use reactstrap

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -1,39 +1,27 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Container, Row, Col, Card, CardBody } from 'reactstrap';
+// import { NavLink } from "react-router-dom";
 
 function About() {
-    return (<main className="flex-shrink-0 content">
-        <div className="container mt-5 mb-5">
-            <div className="row">
-                <div className="col-sm">
-                    <img src="./headshot.jpg" className="img-fluid" alt="Rebecca Eng headshot" data-aos="fade-down"
-                        data-aos-delay="50" />
-                </div>
-                <div className="col-sm d-flex">
-                    <div className="card p-3 border-0" style={{ width: "40rem" }}>
-                        <div className="container">
-                            <h1 className="border-bottom cormorant-font sub-text text-center" data-aos="fade-down"
-                                data-aos-delay="50">Welcome</h1>
-                        </div>
-                        <div className="card-body" data-aos="fade-down" data-aos-delay="50">
+    return (
+        <Container className="mb-3">
+            <Row>
+                <Col lg="6">
+                    <img src="./headshot.jpg" className="img-fluid mt-5" alt="Rebecca Eng headshot" data-aos="fade-down" data-aos-delay="50" />
+                </Col>
+                <Col lg="6">
+                    <Card className="border-0">
+                        <Container className="mt-5">
+                            <h1 className="border-bottom cormorant-font sub-text text-center" data-aos="fade-down" data-aos-delay="50">Welcome</h1>
+                        </Container>
+                        <CardBody data-aos="fade-down" data-aos-delay="50">
                             <p className="card-text cormorant-font">
-                                Hi there! My name is Rebecca Eng. I'm a full stack
-                                software engineer
-                                based in San Francisco, California. <br /> <br /> I have a B.A in International Business and Spanish with a certificate in Full Stack Web Development from UC Berkeley Extension. I have skills in HTML, CSS , JavaScript, and
-                                Python with strengths in problem solving and project management. <br /> <br /> I'm
-                                        passionate about solving user needs and using technology to create a positive social
-                                        impact. I’m excited to combine my web development skills with my background in product
-                                        management to bring a unique perspective on creating products to better serve users.
-                                <br /><br /> If you are a business seeking to build web presence or an employer looking to
-                                hire, you can view my <a href="Eng Rebecca Resume.pdf" target="_blank"
-                                    className="text-body underline">resume</a> and reach me <NavLink to="/contact"
-                                        className="text-body underline">here</NavLink> or at the below links.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
+                                Hi there! My name is Rebecca Eng. I'm a full stack software engineer based in San Francisco, California. <br /> <br /> I have a B.A in International Business and Spanish with a certificate in Full Stack Web Development from UC Berkeley Extension. I have skills in HTML, CSS , JavaScript, and Python with strengths in problem solving and project management. <br /> <br /> I'm passionate about solving user needs and using technology to create a positive social impact. I’m excited to combine my web development skills with my background in product management to bring a unique perspective on creating products to better serve users. <br /><br /> If you are a business seeking to build web presence or an employer looking to hire, you can view my <a href="Eng Rebecca Resume.pdf" target="_blank" className="text-body underline">resume</a> and reach me <a href="/contact" className="text-body underline">here</a> or at the below links.</p>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </Container >
     );
 }
 
