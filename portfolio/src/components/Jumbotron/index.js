2a390fdfbeee23f41cd8279ebc4f74463f13d5cf
reng <reng412@gmail.com>
2020-12-18 14:53:53 -0800

    Update jumbotron component to use reactstrap

--- portfolio/src/components/Jumbotron/index.js
+++ portfolio/src/components/Jumbotron/index.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { Jumbotron, Container } from 'reactstrap';
 import { useLocation } from "react-router-dom";
 import "./style.css";
 
 const jumbotext = new Map([["/portfolio", "PORTFOLIO"], ["/contact", "CONTACT"]])
 
-function Jumbotron() {
+function Jbtrn() {
     const location = useLocation();
     return (
-        <div className="jumbotron jumbotron-fluid bg-img">
-            <div className="container">
-                <h1 className="text-center cormorant-font" id="jumbo-text">{jumbotext.get(location.pathname) || "ABOUT"}</h1>
-            </div>
+        <div>
+            <Jumbotron fluid className="bg-img">
+                <Container fluid>
+                    <h1 className="text-center cormorant-font" id="jumbo-text">{jumbotext.get(location.pathname) || "ABOUT"}</h1>
+                </Container>
+            </Jumbotron>
         </div>
     );
 }
 
-export default Jumbotron;
+export default Jbtrn;
