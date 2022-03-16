07c53b8661ffa673bf1999da066cb954d0cf1a89
reng <reng412@gmail.com>
2022-03-16 15:31:02 -0700

    Update color palette of footer and about page

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -5,10 +5,10 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 text-center">
             <div className="container">
-                <a href="https://www.linkedin.com/in/engrebecca/" target="_blank" rel="noreferrer" className="text-white"><i
+                <a href="https://www.linkedin.com/in/engrebecca/" target="_blank" rel="noreferrer" className="footer-text"><i
                     className="fab fa-linkedin fa-2x mr-3"></i></a>
-                <a href="mailto:reng412@gmail.com" className="text-white"><i className="far fa-envelope fa-2x mr-3"></i></a>
-                <a href="https://github.com/engrebecca" target="_blank" rel="noreferrer" className="text-white"><i
+                <a href="mailto:reng412@gmail.com" className="footer-text"><i className="far fa-envelope fa-2x mr-3"></i></a>
+                <a href="https://github.com/engrebecca" target="_blank" rel="noreferrer" className="footer-text"><i
                     className="fab fa-github fa-2x mr-3"></i></a>
             </div>
         </footer>
