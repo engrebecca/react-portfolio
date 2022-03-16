7219a8ead956ce6134281cfc49f4ed87d0e7aa8e
reng <reng412@gmail.com>
2022-03-16 15:42:26 -0700

    Update color palette of portfolio paragraphs

--- portfolio/src/components/Project/index.js
+++ portfolio/src/components/Project/index.js
@@ -16,7 +16,7 @@ function Project() {
                                 <div className="container text-center project mb-5" key={i}>
                                     <a href={project.deployLink} target="_blank" rel="noreferrer"><img className="card-img-top border border-dark" src={project.image} alt={project.title} data-aos="fade-up" data-aos-delay="50" /></a>
                                     <h4 className="text-center m-4 cormorant-font sub-text">{project.title}</h4>
-                                    <p className="cormorant-font text-center px-5">{project.description}</p>
+                                    <p className="cormorant-font text-center px-5 paragraph-text">{project.description}</p>
                                     <span>
                                         <a href={project.githubLink} className="text-secondary cormorant-font m-3" target="_blank" rel="noreferrer">GitHub</a>
                                         <a href={project.deployLink} className="text-secondary cormorant-font m-3" target="_blank" rel="noreferrer">See It Online</a>
